fc6da87b3776e3187bab539cfbfef74980a8099d
Jeff Seymour and Lauryn Davis <lryndavis@gmail.com>
2016-01-05 14:12:48 -0800

    passes first spec again with different code

--- lib/numberswords.rb
+++ lib/numberswords.rb
@@ -1,12 +1,11 @@
-ones = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
-teens = {10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
-
-# tens{20 => "twenty", 30 => "thirty", 40 => "fourty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80}
 
 class Fixnum
-  define_method(:numbers_to_words) do
-    ones = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
-    words = ones.fetch(self)
-    words
+  less_than_20 = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
+  more_than_20 = {20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
+  define_method(:numbers_to_words) do 
+    if (self <= 19 && self >= 0)
+      return less_than_20.fetch(self)
+
+    end
   end
 end
--- spec/numberswords_spec.rb
+++ spec/numberswords_spec.rb
@@ -1,11 +1,12 @@
 require('rspec')
 require('numberswords')
+require('pry')
 
-describe('string#numbers_to_words') do
+describe('Fixnum#numbers_to_words') do
   it("returns 'one' when 1 is input") do
     expect(1.numbers_to_words()).to(eq("one"))
   end
-  it("returns 'eleven' when 11 is input") do
-    expect(11.numbers_to_words()).to(eq("eleven"))
-  end
+  # it("returns 'eleven' when 11 is input") do
+  #   expect(11.numbers_to_words()).to(eq("eleven"))
+  # end
 end
