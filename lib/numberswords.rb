d36580b4c4228f73c0b5af2e9c7fb62d1bc3bd62
Jeff Seymour and Lauryn Davis <seym1585@gmail.com>
2016-01-05 14:14:19 -0800

    passes second spec

--- lib/numberswords.rb
+++ lib/numberswords.rb
@@ -2,9 +2,10 @@
 class Fixnum
   less_than_20 = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
   more_than_20 = {20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
-  define_method(:numbers_to_words) do 
+  define_method(:numbers_to_words) do
     if (self <= 19 && self >= 0)
       return less_than_20.fetch(self)
+    # if (self >= 20 && self < 100)
 
     end
   end
