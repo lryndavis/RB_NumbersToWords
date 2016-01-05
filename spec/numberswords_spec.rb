631bd056f77aace5c04a309e080af9ec1a738b32
Jeff Seymour and Lauryn Davis <seym1585@gmail.com>
2016-01-05 11:56:05 -0800

    adds second spec

--- spec/numberswords_spec.rb
+++ spec/numberswords_spec.rb
@@ -5,4 +5,7 @@ describe('string#numbers_to_words') do
   it("returns 'one' when 1 is input") do
     expect(1.numbers_to_words()).to(eq("one"))
   end
+  it("returns 'eleven' when 11 is input") do
+    expect(11.numbers_to_words()).to(eq("eleven"))
+  end
 end
