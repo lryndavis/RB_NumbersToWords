2fffc6d6690adb50e874f1e972adf47b91428cf7
Jeff Seymour and Lauryn Davis <lryndavis@gmail.com>
2016-01-05 11:49:50 -0800

    passed first spec

---
+++ lib/numberswords.rb
@@ -0,0 +1,11 @@
+ones = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
+# teens{10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen",}
+# tens{20 => "twenty", 30 => "thirty", 40 => "fourty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80}
+
+class Fixnum
+  define_method(:numbers_to_words) do
+    ones = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
+    words = ones.fetch(self)
+    words
+  end
+end
